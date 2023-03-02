bc2bfa41afa3438f4b3964eff8b86f3d23aac366
Parker Wild <parkerwild96@gmail.com>
2023-03-02 12:10:07 -0500

    Examine marginal benefit of higher market clearing prices.

--- esg.py
+++ esg.py
@@ -22,6 +22,28 @@ here = os.path.dirname(os.path.realpath(__file__))
 supply_file = os.path.join(here, 'data/supply.csv')
 demand_file = os.path.join(here, 'data/demand.csv')
 
+# Portfolio names.
+portfolio_names = [
+    'Bay_Views',
+    'Beachfront',
+    'Big_Coal',
+    'Big_Gas',
+    'East_Bay',
+    'Fossil_Light',
+    'Old_Timers'
+]
+
+# Overhead cost by portfolio. 
+overhead_costs = {
+    'Bay_Views': 90000,
+    'Beachfront': 185000,
+    'Big_Coal': 165000,
+    'Big_Gas': 100000,
+    'East_Bay': 61500,
+    'Fossil_Light': 225000,
+    'Old_Timers': 185000
+}
+
 # Hourly cost by portfolio.
 hrly_costs = {
     'Bay_Views': -5500,
@@ -35,7 +57,9 @@ hrly_costs = {
 
 #--- OUTPUT ---#
 
-dispatch_stack_file = os.path.join(here, 'output/dispatch_stack.csv')
+financials_file = os.path.join(here, 'outputs/financials.csv')
+profitability_curve_file = os.path.join(here, 'outputs/profitability_curve.csv')
+mb_curve_file = os.path.join(here, 'outputs/mb_curve.csv')
 
 #--- FUNCTIONS ---#
 
@@ -52,91 +76,127 @@ def summarize_portfolios(supply):
 
     return
 
-def simulate(supply, demand):
-    '''Baseline scenario for portfolio profitability using merit order dispatch.'''
+def simulate(supply, load, offset=0):
+    '''Calculate portfolio profitability for given load using merit order dispatch.'''
 
-    # Hourly financials by portfolio. 
-    results = pd.DataFrame()
+    # Dispatch instance. 
+    dispatch = supply.copy()
 
-    for _, period in demand.iterrows():
+    # Flag for inframarginal.
+    dispatch['inframarginal'] = False
 
-        # Period. 
-        day = period['day']
-        hour = period['hour']
-        load = period['load']
+    # Flag for marginal. 
+    dispatch['marginal'] = False
 
-        # print('Day, ', day)
-        # print('Hour ', hour)
-        # print('Load ', load)
+    # Set generation to zero.
+    dispatch['gen'] = 0
 
-        # Dispatch instance. 
-        dispatch = supply.copy()
+    # Set revenue to zero. 
+    dispatch['revenue'] = 0
 
-        # Flag for inframarginal.
-        dispatch['inframarginal'] = False
+    # Index of marginal unit.
+    marginal = len(dispatch.loc[dispatch['cumulative'] < load])
 
-        # Flag for marginal. 
-        dispatch['marginal'] = False
+    # Identify inframarginal units.
+    dispatch.loc[:marginal - 1, 'inframarginal'] = True
 
-        # Set generation to zero.
-        dispatch['gen'] = 0
+    # Identify marginal unit.
+    dispatch.loc[marginal, 'marginal'] = True
 
-        # Set revenue to zero. 
-        dispatch['revenue'] = 0
+    # Update generation. 
+    dispatch.loc[dispatch['inframarginal'], 'gen'] = dispatch['mw']
+    dispatch.loc[dispatch['marginal'], 'gen'] = dispatch['mw'] - (dispatch['cumulative'] - load)
+    
+    # Calculate clearing price under zero profit condition (plus offset, if applicable). 
+    cp = dispatch.loc[marginal]['mc'] + offset
 
-        # Index of marginal unit.
-        marginal = len(dispatch.loc[dispatch['cumulative'] < load])
+    # Calculate revenue for each unit. 
+    dispatch['revenue'] = dispatch['gen'] * (cp - dispatch['mc'])
 
-        # Identify inframarginal units.
-        dispatch.loc[:marginal - 1, 'inframarginal'] = True
+    # Calculate revenue for each portfolio.
+    portfolios = dispatch.groupby('portfolio_name', as_index=False).agg({'revenue': 'sum'})
 
-        # Identify marginal unit.
-        dispatch.loc[marginal, 'marginal'] = True
+    # Map hourly cost onto each portfolio.
+    portfolios['cost'] = portfolios['portfolio_name'].map(hrly_costs)
 
-        # Update generation. 
-        dispatch.loc[dispatch['inframarginal'], 'gen'] = dispatch['mw']
-        dispatch.loc[dispatch['marginal'], 'gen'] = dispatch['mw'] - (dispatch['cumulative'] - load)
-        
-        # Calculate clearing price under zero profit condition. 
-        cp = dispatch.loc[marginal]['mc']
+    # Calculate profit for each portfolio. 
+    portfolios['profit'] = portfolios['revenue'] + portfolios['cost']
 
-        # Calculate revenue for each unit. 
-        dispatch['revenue'] = dispatch['gen'] * (cp - dispatch['mc'])
+    return portfolios
 
-        # Calculate revenue for each portfolio.
-        portfolios = dispatch.groupby('portfolio_name', as_index=False).agg({'revenue': 'sum'})
+def profitability(supply):
+    '''Estimate profitability for all portfolios at each level of demand.'''
 
-        # Map hourly cost onto each portfolio.
-        portfolios['cost'] = portfolios['portfolio_name'].map(hrly_costs)
+    # Marginal benefit dataframe. 
+    p = pd.DataFrame(columns=[portfolio_names])
 
-        # Calculate profit for each portfolio. 
-        portfolios['profit'] = portfolios['revenue'] + portfolios['cost']
+    for d in range(1, supply['cumulative'].tail(1).iloc[0] + 1):
 
-        # Add period identifiers. 
-        portfolios['day'] = day
-        portfolios['hour'] = hour
+        # Simulate portfolio profitability with baseline clearing price. 
+        profits = simulate(supply, d, 0)
 
-        # Reformat.
-        portfolios = portfolios[['day', 'hour', 'portfolio_name', 'revenue', 'cost', 'profit']]
+        for _, portfolio in profits.iterrows():
 
-        # Add to results.
-        results = pd.concat([results, portfolios])
+            # Add to marginal benefit dataframe.
+            p.at[d, portfolio['portfolio_name']] = portfolio['profit']
 
-    return results
+    return p
 
-def cashflow(results, portfolio, overhead=225000, r=0.05):
+def mb_curve(supply):
+    '''Estimate marginal benefit of market power for all portfolios at each level of demand.'''
+
+    # Marginal benefit dataframe. 
+    mb = pd.DataFrame(columns=[portfolio_names])
+
+    for d in range(1, supply['cumulative'].tail(1).iloc[0] + 1):
+
+        # Simulate portfolio profitability with baseline clearing price. 
+        profits_t1 = simulate(supply, d, 0)
+
+        # Rename column.
+        profits_t1 = profits_t1.rename(columns={'profit': 'profit_t1'})
+        profits_t1 = profits_t1[['portfolio_name', 'profit_t1']]
+
+        # Simulate portfolio profitability with additional dollar added to clearing price.
+        profits_t2 = simulate(supply, d, 1)
+
+        # Rename column.
+        profits_t2 = profits_t2.rename(columns={'profit': 'profit_t2'})
+        profits_t2 = profits_t2[['portfolio_name', 'profit_t2']]
+
+        # Merge. 
+        profits = profits_t1.merge(profits_t2, on='portfolio_name')
+
+        # Calculate marginal profit of each portfolio for given level of demand. 
+        profits['marginal_benefit'] = profits['profit_t2'] - profits['profit_t1']
+
+        for _, portfolio in profits.iterrows():
+
+            # Add to marginal benefit dataframe.
+            mb.at[d, portfolio['portfolio_name']] = portfolio['marginal_benefit']
+
+    return mb
+
+def bid(day, hour, portfolio, demand, supply):
+    '''Determine optimal bid for given portfolio on day at hour under Cournot competition.'''
+
+    print('Not yet implemented.')
+
+    return
+
+def roi(results, portfolio, overhead=225000, r=0.05):
     '''Calculate cashflow of given portfolio based on expected profitability and interest payments.'''
 
     # Group profits by day. 
-    financials = results.groupby(['day', 'portfolio_name'], as_index=False).agg({'profit': 'sum'})
+    f = results.groupby(['day', 'portfolio_name'], as_index=False).agg({'profit': 'sum'})
 
     # Slice by portfolio.
-    financials = financials.loc[financials['portfolio_name'] == portfolio]
+    f = f.loc[f['portfolio_name'] == portfolio]
 
     # Add columns.
-    financials['owed'] = None
-    financials['payment'] = None
-    financials['cash_flow'] = None
+    f['owed'] = None
+    f['payment'] = None
+    f['cash_flow'] = None
 
     # Financing balance. 
     owed = overhead
@@ -147,19 +207,19 @@ def cashflow(results, portfolio, overhead=225000, r=0.05):
         owed *= (1 + r)
 
         # Track what is owed at the beginning each day.
-        financials.loc[financials['day'] == day, 'owed'] = owed
+        f.loc[f['day'] == day, 'owed'] = owed
 
         # Cash inflow.
-        inflow = financials.loc[financials['day'] == day, 'profit'].iloc[0]
+        inflow = f.loc[f['day'] == day, 'profit'].iloc[0]
 
         # Pay off debt.
         if inflow < 0:
 
             # Update payment.
-            financials.loc[financials['day'] == day, 'payment'] = 0
+            f.loc[f['day'] == day, 'payment'] = 0
 
             # Update cashflow for period. 
-            financials.loc[financials['day'] == day, 'cash_flow'] = inflow
+            f.loc[f['day'] == day, 'cash_flow'] = inflow
 
             # Update owed. 
             owed -= inflow
@@ -167,10 +227,10 @@ def cashflow(results, portfolio, overhead=225000, r=0.05):
         elif inflow <= owed:
 
             # Update payment.
-            financials.loc[financials['day'] == day, 'payment'] = inflow
+            f.loc[f['day'] == day, 'payment'] = inflow
 
             # Update cashflow for period. 
-            financials.loc[financials['day'] == day, 'cash_flow'] = 0
+            f.loc[f['day'] == day, 'cash_flow'] = 0
 
             # Update owed. 
             owed -= inflow
@@ -178,10 +238,10 @@ def cashflow(results, portfolio, overhead=225000, r=0.05):
         elif owed > 0: 
 
             # Update payment.
-            financials.loc[financials['day'] == day, 'payment'] = owed
+            f.loc[f['day'] == day, 'payment'] = owed
 
             # Update cashflow for period. 
-            financials.loc[financials['day'] == day, 'cash_flow'] = inflow - owed
+            f.loc[f['day'] == day, 'cash_flow'] = inflow - owed
 
             # Update owed. 
             owed = 0
@@ -189,19 +249,12 @@ def cashflow(results, portfolio, overhead=225000, r=0.05):
         else:
 
             # Update payment.
-            financials.loc[financials['day'] == day, 'payment'] = 0     
+            f.loc[f['day'] == day, 'payment'] = 0     
 
             # Update cashflow for period. 
-            financials.loc[financials['day'] == day, 'cash_flow'] = inflow     
+            f.loc[f['day'] == day, 'cash_flow'] = inflow     
             
-    return financials
-
-def bid(day, hour, portfolio, demand, supply):
-    '''Determine optimal bid for given portfolio on day at hour.'''
-
-
-
-    return
+    return f
 
 if __name__ == '__main__':
     
@@ -216,22 +269,69 @@ if __name__ == '__main__':
 
     #--- BASELINE ---#
 
-    # Calculate portfolio profits under merit order dispatch.
-    results = simulate(supply, demand)
+    # Hourly financials by portfolio. 
+    financials = pd.DataFrame()
 
-    # Group profits by portfolio. 
-    profits = results.groupby(['portfolio_name'], as_index=False).agg({'profit': 'sum'})
+    for _, period in demand.iterrows():
 
-    #--- FINANCIALS ---#
+        # Period. 
+        day = period['day']
+        hour = period['hour']
+        load = period['load']
 
-    # Overhead. 
-    overhead = 225000
+        # Calculate portfolio profits under merit order dispatch.
+        output = simulate(supply, load, offset=0)
 
-    # Calculate cashflow of desired portfolio given results.
-    financials = cashflow(results, 'Fossil_Light', overhead, 0.05)
+        # Add period identifiers. 
+        output['day'] = day
+        output['hour'] = hour
 
-    # Calculate ROI. 
-    roi = (financials['cash_flow'].sum()/overhead)*100
+        # Add to dataframe.
+        financials = pd.concat([financials, output])
+
+    # Group financials by portfolio. 
+    financials = financials.groupby(['day', 'portfolio_name'], as_index=False).agg({'cost': 'sum', 'revenue': 'sum', 'profit': 'sum'})
+
+    # Write financials to CSV.
+    financials.to_csv(financials_file, index=False)
+
+    # Format financials.
+    print(financials.groupby(['portfolio_name']).agg({'cost': 'sum', 'revenue': 'sum', 'profit': 'sum'}).sort_values(by='profit', ascending=False))
+
+    #--- RETURN ON INVESTMENT ---#
+
+    # ROIs dataframe.
+    rois = pd.DataFrame(columns=['roi']) 
+
+    for portfolio, overhead in overhead_costs.items():
+
+        # Calculate cashflow of desired portfolio given results.
+        output = roi(financials, portfolio, overhead, 0.05)
+
+        # Add to dataframe. 
+        rois.at[portfolio, 'roi'] = output['cash_flow'].sum()/overhead*100
+
+    # Format ROIs. 
+    rois = rois.sort_values(by='roi', ascending=False)
+
+    print(rois)
+
+    #--- PROFITABILITY ---#
+
+    # For each level of demand, which portfolio is most profitable (assuming merit order dispatch)?
+    # p = profitability(supply)
+
+    # Write profitability curve to CSV.
+    # p.to_csv(profitability_curve_file, index=False)
+
+    #--- MARGINAL BENEFITS OF EXERCISING MARKET POWER ---#
+
+    # For each level of demand, which portfolio benefits most from a higher market clearing price (assuming merit order dispatch)? 
+    # mb = mb_curve(supply)
+
+    # Write marginal benefit curve to CSV.
+    # mb.to_csv(mb_curve_file, index=False)
 
     # --- BIDDING ---#
-    
+
+    # Requires game theoretic approach for full implementation.
